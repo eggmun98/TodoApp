7daa57be6c38e115c84e6dd6bb2adf0d6d39a9d2
eggmun98 <eggmun98@gmail.com>
2024-01-10 21:06:01 +0900

    [2024.01.10] Setting 기본 폴더 구조 생성

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import MainScreen from "./screens/MainScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>TodoApp</Text>
-      <StatusBar style="auto" />
-    </View>
+    <MainScreen></MainScreen>
+    // <View style={styles.container}>
+    //   <Text>TodoApp</Text>
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
---
+++ screens/MainScreen.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const MainScreen = () => {
+  return (
+    <View>
+      <Text>MainScreen</Text>
+    </View>
+  );
+};
+
+// ES7+ React/Redux/React-Native snippets 이 확장 플러그인을 이용하면
+// renfs or renf 명령어를 이용하면 리액트 네이티브 컴포넌트 생성 가능
+// rfc 는 일반 리액트 컴포넌트 만들 수 있음
+
+export default MainScreen;
+
+const styles = StyleSheet.create({});
